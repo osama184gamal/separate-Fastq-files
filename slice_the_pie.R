d0846d7b32bfa22fc2bb21b66d32f16e5ab13237
osama184gamal <osama184gamal@gmail.com>
2025-12-31 16:39:47 +0200

    Final result

--- slice_the_pie.R
+++ slice_the_pie.R
@@ -1,10 +1,10 @@
 library(ShortRead)
 
-seperate_fastq = function(r1_file ,r2_file, reads_per_sample ){
+seperate_fastq = function(r1_file ,r2_file, reads_per_sample , sample_file  ){
 output_dir <- "~/chunks"
 dir.create(output_dir, showWarnings = FALSE)
 
-sample_ids <- read.table("/mnt/mydata/project/file.txt", header = FALSE)[,1]
+sample_ids <- read.table(sample_file , header = FALSE)[,1]
 
 stream1 <- FastqStreamer(r1_file, n = reads_per_sample)
 stream2 <- FastqStreamer(r2_file, n = reads_per_sample)
